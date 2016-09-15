c2ba715132be66c2c0eddd876f43605dd4102a29
David Lowndes <david.lowndes@firstb2b.com>
2016-09-15 13:08:37 +0100

    Added User show and article list partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.find(7)
     if @article.save
       flash[:success] = 'Article successfully created'
       redirect_to article_path(@article)

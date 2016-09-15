f2b55ea4e14879745331f5ce338aceb26cf4f438
David Lowndes <david.lowndes@firstb2b.com>
2016-09-15 16:25:45 +0100

    Sessions added, incomplete - Lecture 111

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   
+  #Sessions Controller
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Articles Controller
   resources :articles
   

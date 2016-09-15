bdda2268b811f8ac63ed8e615448395d6f6cb06d
David Lowndes <david.lowndes@firstb2b.com>
2016-09-15 10:48:13 +0100

    User sign up added - Lecture 101

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # Pages Controller
   root 'pages#home'
   get 'about', to: 'pages#about'
   
+  # Articles Controller
   resources :articles
+  
+  # Users Controller
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
 end

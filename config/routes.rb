66c06e14374c961d4bcd1ffa54bd94ab01e0fa68
David Lowndes <david.lowndes@firstb2b.com>
2016-09-16 15:22:16 +0100

    Added categories controller + tests - Lecture 131

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # Pages Controller
+  # Pages Routes
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'deletewebsite', to: 'pages#deletewebsite'
   get 'mememode', to: 'pages#mememode'
   
-  #Sessions Controller
+  #Sessions Routes
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  # Articles Controller
+  # Articles Routes
   resources :articles
   
-  # Users Controller
+  # Users Routes
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  #Categories Routes
+  resources :categories, except: [:destroy]
   
 end
